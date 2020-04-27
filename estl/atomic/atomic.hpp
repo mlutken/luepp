cd13797118a8be9e149f85419cefe5bdd3aa8033
Martin Lütken <martin@lutken.dk>
2020-04-27 13:57:45 +0200

    estl::atomic to have complete public interface in atomic.hpp

--- estl/atomic/atomic.hpp
+++ estl/atomic/atomic.hpp
@@ -17,52 +17,75 @@ namespace estl {
 template <class T>
 class atomic
 {
+public:
+    constexpr atomic() NESTLE_NOEXEPT;
+    constexpr atomic(T desired) NESTLE_NOEXEPT;
 
-
-#if defined(__clang__)
-#elif defined(__GNUG__)
-#include "atomic__gcc.hpp"
-#elif defined(_MSC_VER)
-
+#if (CXX_STANDARD != 98)
+    atomic(const atomic&) = delete;
+    atomic& operator=(const atomic&) = delete;
+    atomic& operator=(const atomic&) volatile = delete;
+#else
+private:
+    atomic(const atomic&);
+    atomic& operator=(const atomic&);
+    atomic& operator=(const atomic&) volatile;
+public:
 #endif
 
+    T operator=( T desired ) NESTLE_NOEXEPT;
+    T operator=( T desired ) volatile NESTLE_NOEXEPT;
 
+    operator T() const NESTLE_NOEXEPT;
+    operator T() const volatile NESTLE_NOEXEPT;
+
+    // Pre increment
+    T operator++() NESTLE_NOEXEPT;
+    T operator++() volatile NESTLE_NOEXEPT;
+
+    // Post increment
+    T operator++(int) NESTLE_NOEXEPT;
+    T operator++(int) volatile NESTLE_NOEXEPT;
+
+    // Pre decrement
+    T operator--() NESTLE_NOEXEPT;
+    T operator--() volatile NESTLE_NOEXEPT;
+
+    // Post decrement
+    T operator--(int) NESTLE_NOEXEPT;
+    T operator--(int) volatile NESTLE_NOEXEPT;
+
+    T operator+=(T arg) NESTLE_NOEXEPT;
+    T operator+=(T arg) volatile NESTLE_NOEXEPT;
+
+    T operator-=(T arg) NESTLE_NOEXEPT;
+    T operator-=(T arg) volatile NESTLE_NOEXEPT;
+
+    T operator&=(T arg) NESTLE_NOEXEPT;
+    T operator&=(T arg) volatile NESTLE_NOEXEPT;
+
+    T operator|=(T arg) NESTLE_NOEXEPT;
+    T operator|=(T arg) volatile NESTLE_NOEXEPT;
+
+    T operator^=(T arg) NESTLE_NOEXEPT;
+    T operator^=(T arg) volatile NESTLE_NOEXEPT;
+
+    bool is_lock_free   () const NESTLE_NOEXEPT;
+    bool is_lock_free   () const volatile NESTLE_NOEXEPT;
+private:
+    // If needed for a given platform/compiler add #ifdefs here!
+    volatile mutable T value_;
 };
+
 
 
 } // END namespace estl
 
-//#if (C_STANDARD == 11)
-//#include <stdatomic.h>
-//#define cesl_sizeof_atomic_flag sizeof(atomic_flag)
-//inline bool cesl_atomic_flag_test_and_set(volatile atomic_flag* obj)
-//{
-//    return atomic_flag_test_and_set(atomic_flag);
-//}
 
-//inline bool cesl_atomic_flag_test_and_set_explicit(volatile atomic_flag* obj, memory_order order)
-//{
-//    return atomic_flag_test_and_set_explicit(obj, order);
-//}
-
-//#elif (C_STANDARD == 99)
-//// If complier gcc: TODO: Other compilers
-//#define cesl_sizeof_atomic_flag 1
-
-//inline bool cesl_atomic_flag_test_and_set(volatile void* obj)
-//{
-//    return __atomic_test_and_set (obj, __ATOMIC_SEQ_CST);
-//}
-
-//inline bool cesl_atomic_flag_test_and_set_explicit(volatile void* obj, int memory_order)
-//{
-//    return __atomic_test_and_set (obj, memory_order);
-//}
-
-
-//#define cesl_atomic_store(object, desired) __sync_lock_test_and_set(object, desired)
-//#define cesl_atomic_load(object) __sync_fetch_and_add(object, 0 )
-
-//#endif
+#if defined(__clang__)
+#elif defined(__GNUG__)
+    #include "atomic__gcc.hpp"
+#elif defined(_MSC_VER)
+#endif
 
 #endif // ESTL_ATOMIC_H
--- estl/atomic/atomic__gcc.hpp
+++ estl/atomic/atomic__gcc.hpp
@@ -1,61 +1,92 @@
-public:
-    constexpr atomic() NESTLE_NOEXEPT {}
+#include <atomic/atomic.hpp>
 
-    constexpr atomic(T desired) NESTLE_NOEXEPT
-        : value_(desired)
-    {
-    }
+namespace estl {
 
-#if (CXX_STANDARD != 98)
-    atomic(const atomic&) = delete;
-    atomic& operator=(const atomic&) = delete;
-    atomic& operator=(const atomic&) volatile = delete;
-#endif
+template <class T>
+inline constexpr atomic<T>::atomic() NESTLE_NOEXEPT {}
 
-    T operator=( T desired ) NESTLE_NOEXEPT           { __atomic_store (&value_, &desired, __ATOMIC_SEQ_CST); return *this; }
-    T operator=( T desired ) volatile NESTLE_NOEXEPT  { __atomic_store (&value_, &desired, __ATOMIC_SEQ_CST); return *this; }
+template <class T>
+constexpr atomic<T>::atomic(T desired) NESTLE_NOEXEPT
+    : value_(desired)
+{
+}
 
+template <class T>
+inline T atomic<T>::operator=( T desired ) NESTLE_NOEXEPT           { __atomic_store (&value_, &desired, __ATOMIC_SEQ_CST); return *this; }
 
-//    inline long InterlockedIncrement(long* p)
-//    {
-//        return __atomic_add_fetch(p, 1, __ATOMIC_SEQ_CST);
-//    }
-    operator T() const NESTLE_NOEXEPT           { T retval; __atomic_load (&value_, &retval, __ATOMIC_SEQ_CST); return retval; }
-    operator T() const volatile NESTLE_NOEXEPT  { T retval; __atomic_load (&value_, &retval, __ATOMIC_SEQ_CST); return retval; }
+template <class T>
+inline T atomic<T>::operator=( T desired ) volatile NESTLE_NOEXEPT  { __atomic_store (&value_, &desired, __ATOMIC_SEQ_CST); return *this; }
 
-    // Pre increment
-    T operator++() NESTLE_NOEXEPT               { return static_cast<T>(__atomic_add_fetch(&value_, 1, __ATOMIC_SEQ_CST)); }
-    T operator++() volatile NESTLE_NOEXEPT      { return static_cast<T>(__atomic_add_fetch(&value_, 1, __ATOMIC_SEQ_CST)); }
-    // Post increment
-    T operator++(int) NESTLE_NOEXEPT            { return static_cast<T>(__atomic_fetch_add(&value_, 1, __ATOMIC_SEQ_CST)); }
-    T operator++(int) volatile NESTLE_NOEXEPT   { return static_cast<T>(__atomic_fetch_add(&value_, 1, __ATOMIC_SEQ_CST)); }
+template <class T>
+inline atomic<T>::operator T() const NESTLE_NOEXEPT           { T retval; __atomic_load (&value_, &retval, __ATOMIC_SEQ_CST); return retval; }
 
-    // Pre decrement
-    T operator--() NESTLE_NOEXEPT               { return static_cast<T>(__atomic_sub_fetch(&value_, 1, __ATOMIC_SEQ_CST)); }
-    T operator--() volatile NESTLE_NOEXEPT      { return static_cast<T>(__atomic_sub_fetch(&value_, 1, __ATOMIC_SEQ_CST)); }
-    // Post decrement
-    T operator--(int) NESTLE_NOEXEPT            { return static_cast<T>(__atomic_fetch_sub(&value_, 1, __ATOMIC_SEQ_CST)); }
-    T operator--(int) volatile NESTLE_NOEXEPT   { return static_cast<T>(__atomic_fetch_sub(&value_, 1, __ATOMIC_SEQ_CST)); }
+template <class T>
+inline atomic<T>::operator T() const volatile NESTLE_NOEXEPT  { T retval; __atomic_load (&value_, &retval, __ATOMIC_SEQ_CST); return retval; }
 
-    T operator+=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_add(&value_, arg, __ATOMIC_SEQ_CST)); }
-    T operator+=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_add(&value_, arg, __ATOMIC_SEQ_CST)); }
+// Pre increment
+template <class T>
+inline T atomic<T>::operator++() NESTLE_NOEXEPT               { return static_cast<T>(__atomic_add_fetch(&value_, 1, __ATOMIC_SEQ_CST)); }
 
-    T operator-=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_sub(&value_, arg, __ATOMIC_SEQ_CST)); }
-    T operator-=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_sub(&value_, arg, __ATOMIC_SEQ_CST)); }
+template <class T>
+inline T atomic<T>::operator++() volatile NESTLE_NOEXEPT      { return static_cast<T>(__atomic_add_fetch(&value_, 1, __ATOMIC_SEQ_CST)); }
 
-    T operator&=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_and(&value_, arg, __ATOMIC_SEQ_CST)); }
-    T operator&=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_and(&value_, arg, __ATOMIC_SEQ_CST)); }
+// Post increment
+template <class T>
+inline T atomic<T>::operator++(int) NESTLE_NOEXEPT            { return static_cast<T>(__atomic_fetch_add(&value_, 1, __ATOMIC_SEQ_CST)); }
 
-    T operator|=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_or(&value_, arg, __ATOMIC_SEQ_CST)); }
-    T operator|=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_or(&value_, arg, __ATOMIC_SEQ_CST)); }
+template <class T>
+inline T atomic<T>::operator++(int) volatile NESTLE_NOEXEPT   { return static_cast<T>(__atomic_fetch_add(&value_, 1, __ATOMIC_SEQ_CST)); }
 
-    T operator^=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_xor(&value_, arg, __ATOMIC_SEQ_CST)); }
-    T operator^=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_xor(&value_, arg, __ATOMIC_SEQ_CST)); }
+// Pre decrement
+template <class T>
+inline T atomic<T>::operator--() NESTLE_NOEXEPT               { return static_cast<T>(__atomic_sub_fetch(&value_, 1, __ATOMIC_SEQ_CST)); }
 
-    bool is_lock_free   () const NESTLE_NOEXEPT            { return __atomic_always_lock_free (sizeof (T), 0); }
-    bool is_lock_free   () const volatile NESTLE_NOEXEPT   { return __atomic_always_lock_free (sizeof (T), 0); }
+template <class T>
+inline T atomic<T>::operator--() volatile NESTLE_NOEXEPT      { return static_cast<T>(__atomic_sub_fetch(&value_, 1, __ATOMIC_SEQ_CST)); }
 
-private:
-    volatile mutable T value_;
+// Post decrement
+template <class T>
+inline T atomic<T>::operator--(int) NESTLE_NOEXEPT            { return static_cast<T>(__atomic_fetch_sub(&value_, 1, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator--(int) volatile NESTLE_NOEXEPT   { return static_cast<T>(__atomic_fetch_sub(&value_, 1, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator+=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_add(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator+=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_add(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator-=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_sub(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator-=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_sub(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator&=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_and(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator&=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_and(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator|=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_or(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator|=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_or(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator^=(T arg) NESTLE_NOEXEPT          {return static_cast<T>(__atomic_fetch_xor(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline T atomic<T>::operator^=(T arg) volatile NESTLE_NOEXEPT {return static_cast<T>(__atomic_fetch_xor(&value_, arg, __ATOMIC_SEQ_CST)); }
+
+template <class T>
+inline bool atomic<T>::is_lock_free   () const NESTLE_NOEXEPT            { return __atomic_always_lock_free (sizeof (T), 0); }
+
+template <class T>
+inline bool atomic<T>::is_lock_free   () const volatile NESTLE_NOEXEPT   { return __atomic_always_lock_free (sizeof (T), 0); }
+
+} // END namespace estl
 
 
