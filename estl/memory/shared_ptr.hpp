8b34b94949e81723aae7e8a467f677c89edc1fcd
Martin Lütken <martin@lutken.dk>
2020-04-27 18:23:57 +0200

    Use atomic counter in in shared_ptr

--- estl/memory/shared_ptr.hpp
+++ estl/memory/shared_ptr.hpp
@@ -7,6 +7,7 @@
 #include <stdexcept>
 #include <algorithm>
 #include <memory>
+#include <atomic/atomic_use.hpp>
 
 // TODO:Temporary/development includes
 #include <iostream>
@@ -14,83 +15,129 @@
 namespace estl {
 template <class T> class weak_ptr;
 
+//typedef unsigned use_counter_type;
+typedef estl_use::atomic<unsigned> use_counter_type;
+
 template <class T>
 class shared_ptr {
-    public:
+public:
 
-        shared_ptr() : ptr(0), count(nil()) { incref(); }
-        ~shared_ptr() { decref(); }
+    shared_ptr()
+        : ptr_(0)
+        , use_count_(nil())
+    {
+        incref();
+    }
 
-        shared_ptr(const shared_ptr<T>& o) : ptr(o.ptr), count(o.count) { incref(); }
-        shared_ptr(T* p) : ptr(p), count(new unsigned(1)) {}
-        explicit shared_ptr(const weak_ptr<T>& w) : ptr(w.ptr), count(w.count) { incref(); }
+    ~shared_ptr()
+    {
+        decref();
+    }
 
-        shared_ptr<T>& operator=(const shared_ptr<T>& o) {
-            if (ptr == o.ptr) return *this;
-            decref();
-            ptr = o.ptr;
-            count = o.count;
-            incref();
-            return *this;
-        }
+    shared_ptr(const shared_ptr<T>& o)
+        : ptr_(o.ptr_)
+        , use_count_(o.use_count_)
+    {
+        incref();
+    }
 
-        T* get() { return ptr; }
-        T* operator->() { return ptr; }
-        T& operator*()  { return *ptr; }
+    shared_ptr(T* p)
+        : ptr_(p)
+        , use_count_(new use_counter_type(1))
+    {
+    }
 
-        const T* get() const { return ptr; }
-        const T* operator->() const { return ptr; }
-        const T& operator*() const { return *ptr; }
+    explicit shared_ptr(const weak_ptr<T>& w)
+        : ptr_(w.ptr_), use_count_(w.count_)
+    {
+        incref();
+    }
 
-        explicit    operator bool() const NESTLE_NOEXEPT { return ptr != nullptr; }
-        bool        operator==(const shared_ptr<T>& o) const { return ptr == o.ptr; }
-        bool        operator!=(const shared_ptr<T>& o) const { return ptr != o.ptr; }
-        bool        operator<(const shared_ptr<T>& o) const { return ptr < o.ptr; }
+    shared_ptr<T>& operator=(const shared_ptr<T>& o)
+    {
+        if (ptr_ == o.ptr_) return *this;
+        decref();
+        ptr_ = o.ptr_;
+        use_count_ = o.use_count_;
+        incref();
+        return *this;
+    }
 
-        unsigned use_count() const { return *count; }
+    T*          get()               { return ptr_;  }
+    T*          operator->()        { return ptr_;  }
+    T&          operator*()         { return *ptr_; }
+
+    const T*    get() const         { return ptr_;  }
+    const T*    operator->() const  { return ptr_;  }
+    const T&    operator*() const   { return *ptr_; }
+
+    explicit    operator bool() const NESTLE_NOEXEPT        { return ptr_ != nullptr;   }
+    bool        operator==(const shared_ptr<T>& o) const    { return ptr_ == o.ptr_;    }
+    bool        operator!=(const shared_ptr<T>& o) const    { return ptr_ != o.ptr_;    }
+    bool        operator<(const shared_ptr<T>& o) const     { return ptr_ < o.ptr_;     }
+
+    unsigned    use_count   () const { return *use_count_; }
 private:
-    T* ptr;
-    unsigned* count; //
-
     /* special case, null pointer (nil-code) */
-    static unsigned* nil() { static unsigned nil_counter(1); return &nil_counter; }
+    static use_counter_type* nil()
+    {
+        static use_counter_type nil_counter(1);
+        return &nil_counter;
+    }
 
-    void decref() { if (--(*count) == 0) { delete ptr; delete count; }}
-    void incref() { ++(*count); }
+    void decref()
+    {
+        if (--(*use_count_) == 0) {
+            delete ptr_;
+            delete use_count_;
+        }
+    }
+
+    void incref()
+    {
+        ++(*use_count_);
+    }
 
     friend class weak_ptr<T>;
 
+    // --- PRIVATE: Member data ---
+    T* ptr_;
+    use_counter_type* use_count_;
 };
 
 template <class T>
 class weak_ptr {
-        T* ptr;
-        unsigned* count;
+public:
+    friend class shared_ptr<T>;
 
-        friend class shared_ptr<T>;
+    weak_ptr()
+        : ptr_(0), count_(shared_ptr<T>::nil())
+    {}
 
-    public:
+    explicit weak_ptr( const shared_ptr<T>& s)
+        : ptr_(s.ptr_), count_(s.use_count_)
+    {}
 
-        weak_ptr() : ptr(0), count(shared_ptr<T>::nil()) {}
-        explicit weak_ptr( const shared_ptr<T>& s) : ptr(s.ptr), count(s.count) {}
-
-        shared_ptr<T> lock() const { return shared_ptr<T>(*this); }
+    shared_ptr<T> lock() const { return shared_ptr<T>(*this); }
 
 
-        T* get() { return ptr; }
-        T* operator->() { return ptr; }
-        T& operator*()  { return *ptr; }
+    T*          get()               { return ptr_;  }
+    T*          operator->()        { return ptr_;  }
+    T&          operator*()         { return *ptr_; }
 
-        const T* get() const { return ptr; }
-        const T* operator->() const { return ptr; }
-        const T& operator*() const { return *ptr; }
+    const T*    get() const         { return ptr_;  }
+    const T*    operator->() const  { return ptr_;  }
+    const T&    operator*() const   { return *ptr_; }
 
-        bool operator==(const shared_ptr<T>& o) const { return ptr == o.ptr; }
-        bool operator!=(const shared_ptr<T>& o) const { return ptr != o.ptr; }
-        bool operator<(const shared_ptr<T>& o) const { return ptr < o.ptr; }
+    bool        operator==(const shared_ptr<T>& o) const { return ptr_ == o.ptr_; }
+    bool operator!=(const shared_ptr<T>& o) const { return ptr_ != o.ptr_; }
+    bool operator<(const shared_ptr<T>& o) const { return ptr_ < o.ptr_; }
 
-        unsigned use_count() const { return *count; }
-
+    unsigned use_count() const { return *count_; }
+private:
+    // --- PRIVATE: Member data ---
+    T* ptr_;
+    use_counter_type* count_;
 };
 
 }
