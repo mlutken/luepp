0344d102a6d8cb18206bf4bc96d13d5099ad31f2
Martin Lütken <martin@lutken.dk>
2023-05-24 13:11:47 +0200

    Added missing mutex__browser.cpp

---
+++ estl/concurrent/mutex__browser.cpp
@@ -0,0 +1,34 @@
+#include "mutex.hpp"
+
+namespace estl {
+
+mutex::mutex() noexcept
+{
+    pthread_mutex_init(&pthread_mutex_, nullptr);
+}
+
+mutex::~mutex()
+{
+    pthread_mutex_destroy(&pthread_mutex_);
+}
+
+void mutex::lock()
+{
+    pthread_mutex_lock(&pthread_mutex_);
+}
+
+bool mutex::try_lock()
+{
+    return pthread_mutex_trylock(&pthread_mutex_) == 0;
+}
+
+void mutex::unlock()
+{
+    pthread_mutex_unlock(&pthread_mutex_);
+}
+
+
+} // END namespace estl
+
+
+
