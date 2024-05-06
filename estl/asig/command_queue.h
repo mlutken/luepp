122349b424ec7191f2780526956b93b8937f7c74
Martin Lütken <martin.lutken@cobhamsatcom.com>
2024-05-06 17:33:46 +0200

    First simple callback supported in command

--- estl/asig/command_queue.h
+++ estl/asig/command_queue.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <iostream>
 #include <mutex>
 #include <functional>
 #include <concurrent/srsw_fifo.hpp>
+
 // https://en.cppreference.com/w/cpp/utility/functional/invoke
 // https://codereview.stackexchange.com/questions/221828/c17-implementation-of-stdinvoke
 // https://stackoverflow.com/questions/46388524/when-to-use-stdinvoke-instead-of-simply-calling-the-invokable
@@ -14,16 +16,47 @@ class command_queue
 public:
     explicit command_queue(size_t queue_size = 128);
 
-    void    push            (std::function<void ()>&&  command_fun);
+    void    push_call_void  (std::function<void ()>&&  command_fun);
+
+    template<class ReturnType>
+    void    push_call       (std::function<ReturnType ()>&&  command_fun,
+                             std::function<void (const ReturnType&)>&&  result_callback_fun)
+    {
+        auto fn = [=]() {
+            std::cerr << "Executing with return type and callback\n";
+            auto cmd_ret_val = command_fun();
+            result_callback_fun(cmd_ret_val);
+
+        };
+        push_call_void(std::move(fn));
+    }
 
 
-    template<class Callable, typename ... Args>
-    void    call            (Callable function, Args... args)
+    template<class CommandCallable, typename ... CommandArgs >
+    void call_void (CommandCallable&& function, const CommandArgs&... args)
     {
         auto fn = [=]() {
             std::invoke(function, args...);
         };
-        push(std::move(fn));
+        push_call_void(std::move(fn));
+    }
+
+
+    template<typename ReturnType,
+             class ResultCallable,
+             class CommandCallable, typename ... CommandArgs >
+    void call ( ResultCallable result_callback_fun,
+                CommandCallable command_fun, CommandArgs... command_args
+                )
+    {
+        auto command_fn = [=]() -> ReturnType {
+            return std::invoke(command_fun, command_args...);
+        };
+
+        auto result_callback_fn = [=](const ReturnType& cmd_return_value){
+            return std::invoke(result_callback_fun, cmd_return_value );
+        };
+        push_call<ReturnType>(std::move(command_fn),  std::move(result_callback_fn));
     }
 
     bool    execute_next    ();
--- estl/asig/playground/commands_playground.cpp
+++ estl/asig/playground/commands_playground.cpp
@@ -15,6 +15,13 @@ struct MyClass
     void member_function(int some_number)
     {
         cerr << name_ << "::member_function(" << some_number << ")\n";
+
+    }
+
+    int mul2(int some_number)
+    {
+        cerr << name_ << "::mul2(" << some_number << ")\n";
+        return 2*some_number;
     }
 
     std::string name_;
@@ -23,6 +30,11 @@ struct MyClass
 void free_function(int some_number)
 {
     cerr << "Hello free_function(" << some_number << ")\n";;
+}
+
+void free_callback(int some_number)
+{
+    cerr << "Hello free_callback(" << some_number << ")\n";;
 }
 
 command_queue queue1{256};
@@ -34,9 +46,22 @@ int main()
     cerr << "queue1.capacity()   : "  << queue1.capacity() << "\n";
     cerr << "queue1.size()       : "  << queue1.size() << "\n";
 
-    queue1.push([](){cerr << "Hello from push\n"; });
-    queue1.call(free_function, 12);
-    queue1.call(&MyClass::member_function, &my_class, 23);
+    queue1.push_call_void([](){cerr << "Hello from push\n"; });
+    queue1.call_void(free_function, 12);
+    queue1.call_void(&MyClass::member_function, &my_class, 23);
+
+
+    auto command_fn = [=]() -> int {
+        return my_class.mul2(25);
+    };
+
+    auto result_callback_fn = [=](const int& cmd_return_value){
+        free_callback(cmd_return_value);
+    };
+    queue1.push_call<int>(std::move(command_fn),  std::move(result_callback_fn));
+
+
+    queue1.call<int>(free_callback, &MyClass::mul2, &my_class, 30);
 
 
     while (!queue1.empty()) {
